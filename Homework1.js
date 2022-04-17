f292ac035e9f062448845a84cf86ddc6154d18de
Anush Martirosyan <50011776+MartAnush@users.noreply.github.com>
2022-04-18 01:09:58 +0400

    Update Homework1.js

--- Homework1.js
+++ Homework1.js
@@ -1,4 +1,4 @@
-1. // Given an object. Invert it (keys become values and values become keys). If there is
+ // 1. Given an object. Invert it (keys become values and values become keys). If there is
 // more than key for that given value create an array.
 
 let obj = {
@@ -23,3 +23,20 @@ let obj = {
             newObj[obj[key]] = key
         }
     }
+
+
+
+//2. Given two objects. Write a function that performs shallow compare.
+
+function shallowCompare(a,b) {
+	if (Object.keys(a).length !== Object.keys(b).length || Object.keys(a).length === 0){
+    return false
+	} 
+
+  for (let key in a) {
+    if(a[key] !== b[key]){
+      return  false
+    }
+   return true
+  }
+}
