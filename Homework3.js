fa3fbe3e08c8235cecbacfdc81089950268a2e12
Anush Martirosyan <50011776+MartAnush@users.noreply.github.com>
2022-05-03 01:27:17 +0400

    Update Homework3.js

--- Homework3.js
+++ Homework3.js
@@ -1,8 +1,25 @@
-// 1. Given an array. Write a recursive function that removes the first element and returns the given
-// array. (without using arr.unshift(),assign second element to first, third element to second...)
+ 1. Given an array. Write a recursive function that removes the first element and returns the given
+ array. (without using arr.unshift(),assign second element to first, third element to second...)
 
 function removeFirst(arr){
     arr.splice(0,1)
     return arr
     
+}
+
+
+2.Given an array of nested arrays. Write a recursive function that flattens it. (Hint create function
+that concats arrays).
+
+let arr = []
+
+function  flatten (a) {
+	for (let i =0; i<a.length; i++) {
+        if (Array.isArray(a[i])) {
+            flatten(a[i]);
+        } else {
+            arr.push(a[i]);
+        }
+	} 
+    return arr;
 }
