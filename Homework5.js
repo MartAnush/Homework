5088fafcfeb95eddbcb0aa9e51ee2d14338fed36
Anush Martirosyan <50011776+MartAnush@users.noreply.github.com>
2022-05-24 18:25:45 +0400

    Update Homework5.js

--- Homework5.js
+++ Homework5.js
@@ -14,3 +14,46 @@ function getAllSubsets(array,n) {
     const filtered = subsets.filter(ar => ar.length == n)
     return filtered;
 }
+
+
+1. Create a function that builds a tree.
+const treeNodes = [
+    {id: 'root', children: [1, 2]},
+    {id: 1, children: [3,]},
+    {id: 2, children: [4, 5]},
+    {id: 3, children: [6]},
+    {id: 4, children: [7]},
+    {id: 5, children: []},
+    {id: 6, children: []},
+    {id: 7, children: [8, 9]},
+    {id: 8, children: []},
+    {id: 9, children: [10]},
+    {id: 10, children: []},
+]
+
+
+//Davit jan kisata 
+
+
+var tree = {}
+
+function createTree(arr){
+  for(let i = 0; i< arr.length; i++) {
+    if(arr[i].id != undefined && arr[i].id == 'root') {
+    	tree[arr[i]] = arr[i].id
+    }
+      for(let j = 0; j < arr[i].children.length; j++) {
+    		if(isExist(arr[i].children[j])){
+        tree[arr[i].children[j]] = {}
+        }
+      }
+  }
+  return tree   
+}
+
+function isExist(id) {
+  return treeNodes.some(function(el) {
+    return el.id === id;
+  }); 
+}
+
