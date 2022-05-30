4ad4932778525268bfdcb8caf48c123f427ca89f
Anush Martirosyan <50011776+MartAnush@users.noreply.github.com>
2022-05-31 02:05:55 +0400

    Update Homework6.js

--- Homework6.js
+++ Homework6.js
@@ -1,3 +1,5 @@
+1.Get array of node ids from tree (keys). 
+
 function createArray(obj, result = []){
     for (let key of Object.keys(obj)){
         result.push(+key)
@@ -5,6 +7,6 @@ function createArray(obj, result = []){
             createArray(obj[key], result)
       } 
     }
-    return result
-    }
+  return result
+ }
     
