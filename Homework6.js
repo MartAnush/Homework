3de5ea9a128698123b1774059a1e59e038f79795
Anush Martirosyan <50011776+MartAnush@users.noreply.github.com>
2022-06-01 00:50:19 +0400

    Update Homework6.js

--- Homework6.js
+++ Homework6.js
@@ -10,3 +10,32 @@ function createArray(obj, result = []){
   return result
  }
     
+
+
+3. Implement binary search.
+
+function binaryExtract(array, value) {
+  newArray = array.sort(function(a, b){return a - b});
+  let mid = newArray[Math.floor((newArray.length - 1) / 2)];
+
+if (mid == value) {
+	return value;
+}
+
+if (newArray.lenght == 1 && newArray[0] != value) {
+	return -1;
+}
+else if (newArray.lenght == 1 && newArray[0] == value) {
+	return value;
+}
+
+ if (value >= mid ) {
+		newArray = newArray.slice(newArray.indexOf(mid) +1)
+  return binaryExtract(newArray, value)
+} else if(value < mid) {
+		newArray = newArray.slice(0, - newArray.indexOf(mid) -1)
+ 	return binaryExtract(newArray, value)
+}
+return -1;
+}
+
